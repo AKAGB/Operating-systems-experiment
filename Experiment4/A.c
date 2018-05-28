4a0a7921dd1a50922a7495553cdcbe883431623b
AKAGB <374118168@qq.com>
2018-05-29 01:00:46 +0800

    Ex4 1 finish

--- Experiment4/A.c
+++ Experiment4/A.c
@@ -9,39 +9,57 @@
 #define MSGKEY1 75
 #define MSGKEY2 76
 
-struct msgform {
-    long mtype;
-    char mtext[1000];
-} msg1, msg2;
+char msg1[1024];
+char msg2[1024];
 
 int msgqid1;
 int msgqid2;
 
 const char *quit = "quit";
 
+void quit_parent(int sig) {
+    // 父进程关闭队列后退出
+    msgctl(msgqid1, IPC_RMID,0);
+    exit(0);
+}
+
+void quit_child(int sig) {
+    // 父进程关闭队列后退出
+    msgctl(msgqid2, IPC_RMID,0);
+    exit(0);
+}
+
 int main()
 {
     pid_t pid;
     if ((pid=fork()) == 0) {
         // 子进程rcv
-        // while(1) {}
-        // printf("ChildA start\n");
-        // do {
-        //     msgrcv(msgqid1, &msg1, sizeof(msg1),0,0);
-        //     printf("(Process A) received: %s\n", msg1.mtext);
-        // } while (strcmp(msg1.mtext, quit));
-        // msgctl(msgqid1, IPC_RMID, 0);
-        // // 结束父进程
-        // kill(getppid(), SIGQUIT);
+        signal(SIGINT, quit_child);
+        msgqid2 = msgget(MSGKEY2, 0777|IPC_CREAT);
+        do {
+            msgrcv(msgqid2, &msg2, sizeof(msg2), 0, 0);
+            printf("(Process A) received: %s\n", msg2);
+        }while(strcmp(msg2, quit) != 0);
+        msgctl(msgqid2, IPC_RMID,0);
+
+        // 结束父进程
+        kill(getppid(), SIGINT);
+
+        exit(0);
     }
     else {
         // 父进程snd
+        signal(SIGINT, quit_parent);
         msgqid1 = msgget(MSGKEY1, 0777|IPC_CREAT);
         do {
-            scanf("%s", msg1.mtext);
-            msgsnd(msgqid1, &msg1, sizeof(msg1),0);
-            printf("(Process A) sent: %s\n", msg1.mtext);
-        } while(strcmp(msg1.mtext, quit) != 0);
+            scanf("%s", msg1);
+            printf("(Process A) sent: %s\n", msg1);
+            msgsnd(msgqid1, &msg1, sizeof(msg1), 0);
+        } while(strcmp(msg1, quit) != 0);
+        msgctl(msgqid1, IPC_RMID,0);
+        
+        // 结束子进程
+        kill(pid, SIGINT);
     }
     return 0;
 }
--- Experiment4/B.c
+++ Experiment4/B.c
@@ -9,10 +9,8 @@
 #define MSGKEY1 75
 #define MSGKEY2 76
 
-struct msgform {
-    long mtype;
-    char mtext[1000];
-} msg1, msg2;
+char msg1[1024];
+char msg2[1024];
 
 int msgqid1;
 int msgqid2;
@@ -23,28 +21,28 @@ int main()
 {
     pid_t pid;
     if ((pid=fork()) == 0) {
-        sleep(1);
-        printf("CHildB\n");
         // 子进程rcv
         msgqid1 = msgget(MSGKEY1, 0777);
         do {
-            msgrcv(msgqid1, &msg1, sizeof(msg1),0,0);
+            msgrcv(msgqid1, &msg1, sizeof(msg1), 0, 0);
             printf("(Process B) received: %s\n", msg1);
-        } while(strcmp(msg1.mtext, "quit") != 0);
-        msgctl(msgqid1, IPC_RMID, 0);
+        }while(strcmp(msg1, quit) != 0);
+
         // 结束父进程
-        // kill(getppid(), SIGQUIT);
+        kill(getppid(), SIGINT);
+        exit(0);
     }
     else {
         // 父进程snd
-        // while (1) {}
-        // do {
-        //     scanf("%s", msg1);
-        //     printf("(Process B) sent: %s\n", msg1.mtext);
-        //     msgsnd(msgqid1, &msg1, sizeof(msg1), 0);
-        // } while(strcmp(msg1.mtext, "quit") != 0);
-        // // 结束子进程
-        // kill(pid, SIGQUIT);
+        msgqid2 = msgget(MSGKEY2, 0777);
+        do {
+            scanf("%s", msg2);
+            printf("(Process B) sent: %s\n", msg2);
+            msgsnd(msgqid2, &msg2, sizeof(msg2), 0);
+        } while(strcmp(msg2, quit) != 0);
+        
+        // 结束子进程
+        kill(pid, SIGINT);
     }
     return 0;
 }
