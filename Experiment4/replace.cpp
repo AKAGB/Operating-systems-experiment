8b68b8d5103821d886f0c14aba19c1188c7fceb9
AKAGB <374118168@qq.com>
2018-05-30 02:16:39 +0800

    replace v2

--- Experiment4/replace.cpp
+++ Experiment4/replace.cpp
@@ -4,20 +4,51 @@
 
 using namespace std;
 
+string sub, dst;
+
 string replace(string src, string sub, string dst)
 {
-    int index = src.find(sub), len = sub.length();
+    if (sub == dst) return src;
+    int index = src.find(sub), len = sub.length(), dlen = dst.length();
     while (index != -1) {
         src = src.substr(0, index) + dst + src.substr(index + len);
-        index = src.find(sub);
+        index = src.find(sub, index + dlen);
     }
     return src;
 }
 
+void file_replace_str(void * filename)
+{
+    string fpath = *((string *)filename);
+    // Open file
+    string src;
+    fstream file(fpath, ios::in);
+    if (file) {
+        file >> src;
+        file.close();
+        file.open(fpath, ios::out);
+        file << replace(src, sub, dst) << endl;
+        file.close();
+    }
+    else {
+        cout << "No such file." << endl;
+    }
+}
+
 int main() 
 {
-    string src = "abcaabbcabcaucdabc", sub = "abc", dst = "ntr";
-    cout << replace(src, sub, dst) << endl;
+    // Input
+    string fpath1, fpath2;
+    cout << "input file 1 path: ";
+    cin >> fpath1;
+    cout << "input file 2 path: ";
+    cin >> fpath2;
+    cout << "input string replace from: ";
+    cin >> sub;
+    cout << "input string replace to: ";
+    cin >> dst;
+
+    file_replace_str(&fpath1);
 
     return 0;
 }
