4d46546c75a06330ed9ec74253a8a40df95fe36f
AKAGB <374118168@qq.com>
2018-07-03 17:56:47 +0800

    ex6 delete -all

--- Experiment6/file_system.c
+++ Experiment6/file_system.c
@@ -9,6 +9,8 @@
 
 #define LENGTH 100
 
+const char *tab = "   ";
+
 void list_info(struct dirent *dirp, char *path) {
     struct stat buf;
     char tmpBuf[LENGTH];
@@ -31,22 +33,57 @@ void list_info(struct dirent *dirp, char *path) {
     chdir(now);
 }
 
+void show_dir(char *path) {
+    
+}
+
+void delete_dir(char *dir) {
+    char now[LENGTH] = {0};
+    getcwd(now, LENGTH);
+    chdir(dir);
+
+    DIR *dirptr;
+    struct dirent *dirp;
+
+    if ((dirptr = opendir(".")) != NULL) {
+        while ((dirp = readdir(dirptr)) != NULL) {
+            // printf("%s\n", dirp->d_name);
+            if (dirp->d_type == 4) {
+                if (strcmp(dirp->d_name, "..") != 0 && strcmp(dirp->d_name, ".") != 0) {
+                    delete_dir(dirp->d_name);
+                    rmdir(dirp->d_name);
+                }  
+            }
+            else {
+                printf("Delete %s/%s\n", dir, dirp->d_name);
+                remove(dirp->d_name);
+            }
+        }
+    }
+
+    chdir(now);
+}
+
 int main()
 {  
     
     while (1) {
-        char cmd[LENGTH]={0}, ins[LENGTH]={0}, obj[LENGTH]={0};
+        char cmd[LENGTH]={0}, ins[LENGTH]={0}, obj[LENGTH]={0}, opt[LENGTH]={0};
         int fd;
         DIR *dirptr;
         struct dirent *dirp;
 
         printf("> ");
         fgets(cmd, LENGTH, stdin);
-        sscanf(cmd, "%s %s", ins, obj);
+        sscanf(cmd, "%s %s %s", ins, obj, opt);
         // printf("cmd = %s\nins = %s\nobj = %s\n", cmd, ins, obj);
 
         if (strcmp(ins, "create") == 0) {
             // 创建文件
+            if (strlen(obj) == 0) {
+                printf("Please input the file name.\n");
+                continue;
+            }
             // printf("Create a file\n");
             if ((fd = open(obj, O_RDONLY)) != -1) {
                 // printf("fd = %d, File exist\n", fd);
@@ -61,7 +98,16 @@ int main()
         }
         else if (strcmp(ins, "delete") == 0) {
             // 删除文件
-            if ((fd = open(obj, O_RDONLY)) != -1) {
+            if (strlen(obj) == 0) {
+                printf("Please input the file name.\n");
+                continue;
+            }
+            if (strcmp(obj, "-all") == 0) {
+                if (strlen(obj) == 0) sprintf(obj, "%c", '.');
+                delete_dir(opt);
+                rmdir(opt);
+            }
+            else if ((fd = open(obj, O_RDONLY)) != -1) {
                 // printf("fd = %d, File exist\n", fd);
                 close(fd);
                 remove(obj);
@@ -73,6 +119,10 @@ int main()
         }
         else if (strcmp(ins, "mkdir") == 0) {
             // 创建目录
+            if (strlen(obj) == 0) {
+                printf("Please input the dir name.\n");
+                continue;
+            }
             if ((dirptr = opendir(obj)) == NULL) {
                 mkdir(obj, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
             }
@@ -83,12 +133,18 @@ int main()
         }
         else if (strcmp(ins, "rmdir") == 0) {
             // 删除目录
+            if (strlen(obj) == 0) {
+                printf("Please input the dir name.\n");
+                continue;
+            }
             if ((dirptr = opendir(obj)) == NULL) {
                 printf("The dir %s does not exists.\n", obj);
             }
             else {
                 closedir(dirptr);
-                rmdir(obj);
+                if (rmdir(obj) == -1) {
+                    printf("rmdir can not delete non-empty dir.\n");
+                }
             }
         }
         else if (strcmp(ins, "list") == 0) {
@@ -103,6 +159,9 @@ int main()
                 }
                 closedir(dirptr);
             }
+        }
+        else if (strcmp(ins, "tree")) {
+            if (strlen(obj) == 0) sprintf(obj, "%c", '.');
         }
         else {
             if (strlen(cmd) != 1) printf("No such command\n");
